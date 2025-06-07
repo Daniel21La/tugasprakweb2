74fe91dc3f66bc63a04e705ba663d273de960b50
Daniel21La <daniellaroce@gmail.com>
2025-06-07 17:50:09 +0700

    Delete mahasiswa.blade.php

--- mahasiswa.blade.php
+++
@@ -1,6 +0,0 @@
- @extends('layout.template') 
- @section('judul', 'Halaman Mahasiswa') 
- @section('isi') 
-     <h3>Ini halaman view mahasiswa</h3> 
-     <a href="/biodata">Biodata</a> 
- @endsection
